a9ce5c1077d14fafcfd75cde8116b5121710529c
Davide Bianchi <davide.bianchi@mondora.com>
2016-06-08 15:37:18 +0200

    Changed LOG_LEVEL declaration.

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
 export const NODE_ENV = process.env.NODE_ENV || "development";
-export const LOG_LEVEL = process.env.LOG_LEVEL || "info";
+export const LOG_LEVEL = process.env.DEBUG ? "debug" : "info";
